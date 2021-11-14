ad82b92a0c57d27262c73f82f9cc086cb76bfd34
Nguyễn Đình Hùng <nguyendinhhung152@gmail.com>
2021-11-14 21:29:34 +0700

    fix category and modify header

--- src/components/AccountHeader/AccountHeader.tsx
+++ src/components/AccountHeader/AccountHeader.tsx
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import './AccountHeader.scss';
 import { FiUser, FiSettings } from 'react-icons/fi';
 import { MdOutlineExitToApp } from 'react-icons/md';
+import { BsBoxArrowInRight } from 'react-icons/bs';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/rootReducer';
+import { LoginModal } from '../LoginModal/LoginModal';
 
 export const AccountHeader = () => {
   const [showOptions, setShowOptions] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const { isUser } = useSelector((state: RootState) => state.userSlice);
 
   const handleOnEnter = () => {
     setShowOptions(true);
@@ -16,24 +22,36 @@ export const AccountHeader = () => {
 
   return (
     <div className="account-header" onMouseEnter={handleOnEnter} onMouseLeave={handleOnLeave}>
-      <div className="account-header__label">
-        <FiUser size={25} color={'#F7941E'} />
-        <span>TÀI KHOẢN</span>
-      </div>
-      <div
-        className={`account-header__options ${showOptions ? 'account-header__options--show' : ''}`}
-      >
-        <div className="account-header__list">
-          <div className="account-header__option">
-            <FiSettings size={20} />
-            <span>Bảng điều khiển của khách hàng</span>
+      {!isUser ? (
+        <div className="account-header__label">
+          <BsBoxArrowInRight size={25} color={'#F7941E'} onClick={() => setIsOpen(true)} />
+          <LoginModal isOpen={isOpen} setIsOpen={setIsOpen} />
+          <span>ĐĂNG NHẬP</span>
+        </div>
+      ) : (
+        <div>
+          <div className="account-header__label">
+            <FiUser size={25} color={'#F7941E'} />
+            <span>TÀI KHOẢN</span>
           </div>
-          <div className="account-header__option">
-            <MdOutlineExitToApp size={20} />
-            <span>Thoát</span>
+          <div
+            className={`account-header__options ${
+              showOptions ? 'account-header__options--show' : ''
+            }`}
+          >
+            <div className="account-header__list">
+              <div className="account-header__option">
+                <FiSettings size={20} />
+                <span>Bảng điều khiển của khách hàng</span>
+              </div>
+              <div className="account-header__option">
+                <MdOutlineExitToApp size={20} />
+                <span>Thoát</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Cart.scss';
+import { BsCart3 } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 Cart.propTypes = {};
 
 function Cart() {
   return (
     <div className="btn-cart" id="cart">
-      <a title="My cart" href="/">
-        Cart
-      </a>
-      <span className="notify notify-right">7</span>
-      <div className="cart-des">GIỎ HÀNG</div>
+      <Link title="My cart" to="/cart"> <div className="btn-cart__label">
+        <BsCart3 size={25} color={'#F7941E'} />
+        <span>Giỏ hàng</span>
+      </div></Link>
+     
+      <span className="notify notify-right">1</span>
       <div className="cart-block">
         <div className="total-cart">
           <div className="cart-img">
